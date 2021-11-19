c246b8b7942baf962e1e129b7799defda6c32076
Brandon Piper <brandonpiper1@gmail.com>
2021-11-19 21:04:48 +0000

    Initial commit

---
+++ NewOld/Old/Scripts/CameraSize.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class CameraSize : MonoBehaviour
+{
+    // Set this to the in-world distance between the left & right edges of your scene.
+    public float sceneWidth = 10;
+
+    Camera camera;
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float unitsPerPixel = sceneWidth / Screen.width;
+
+        float desiredHalfHeight = 0.5f * unitsPerPixel * Screen.height;
+
+        camera.orthographicSize = desiredHalfHeight;
+    }
+}
+
---
+++ NewOld/Old/Scripts/mtouch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class mtouch : MonoBehaviour
+{
+    public static cursorwin pointer;
+    public Texture2D mouse, click, error, middleclick;
+    public bool setter;
+
+    private bool IsMouseOverUI()
+    {
+        return EventSystem.current.IsPointerOverGameObject();
+    }
+
+    void Start()
+    {
+        if (setter)
+        {
+            Cursor.SetCursor(mouse, Vector2.zero, CursorMode.Auto);
+
+            pointer = this;
+        }
+    }
+
+    private void Update()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            Cursor.SetCursor(click, Vector2.zero, CursorMode.Auto);
+        }
+    }
+}
